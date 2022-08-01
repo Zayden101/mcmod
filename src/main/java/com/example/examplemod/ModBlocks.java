76893929e22f3a65554c476b80142eb86edaefcd
League Student <league@iMac-14.attlocal.net>
2022-08-01 15:35:58 -0700

    Initial commit

---
+++ src/main/java/com/example/examplemod/ModBlocks.java
@@ -0,0 +1,25 @@
+package com.example.examplemod;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.*;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ExampleMod.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MODID);
+    public static final RegistryObject<Item> ROCK = ITEMS.register("rock", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Block> FANCY_ORE = BLOCKS.register("fancy_ore", () -> new Block(AbstractBlock.Properties.of(Material.STONE)));
+    public static final RegistryObject<BlockItem> FANCY_ORE_ITEM = ITEMS.register("fancy_ore_item", () -> new BlockItem(FANCY_ORE.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
+    public static final RegistryObject<Item> MY_AXE = ITEMS.register("my_axe", () -> new AxeItem(ItemTier.NETHERITE, 15, -2.8f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_PICKAXE = ITEMS.register("my_pickaxe", () -> new PickaxeItem(ItemTier.NETHERITE, 2, -1.0f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_SHOVEL = ITEMS.register("my_shovel", () -> new ShovelItem(ItemTier.NETHERITE, 3, -2.0f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_SWORD = ITEMS.register("my_sword", () -> new SwordItem(ItemTier.NETHERITE, 8, -0.5f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_HOE = ITEMS.register("my_hoe", () -> new HoeItem(ItemTier.NETHERITE, 1, -1.0f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_BOW = ITEMS.register("my_bow", () -> new BowItem(new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+
+}
