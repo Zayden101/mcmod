5e04d7642b1170e80dc5757447a53bc4416a2d35
League Student <league@iMac-14.attlocal.net>
2022-08-02 15:46:37 -0700

    Aug 2

--- src/main/java/com/example/examplemod/ModBlocks.java
+++ src/main/java/com/example/examplemod/ModBlocks.java
@@ -21,5 +21,15 @@ public class ModBlocks {
     public static final RegistryObject<Item> MY_SWORD = ITEMS.register("my_sword", () -> new SwordItem(ItemTier.NETHERITE, 8, -0.5f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
     public static final RegistryObject<Item> MY_HOE = ITEMS.register("my_hoe", () -> new HoeItem(ItemTier.NETHERITE, 1, -1.0f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
     public static final RegistryObject<Item> MY_BOW = ITEMS.register("my_bow", () -> new BowItem(new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> DAMAGE = ITEMS.register("damage", () -> new HoeItem(ItemTier.NETHERITE, 100, -0.1f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> MY_CROSSBOW = ITEMS.register("my_crossbow", () -> new CrossbowItem(new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> THROWABLE_ARROW = ITEMS.register("throwable_arrow", () -> new BowItem(new Item.Properties().tab(ItemGroup.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Block> NETHER_BLOCK = BLOCKS.register("nether_block", () -> new Block(AbstractBlock.Properties.of(Material.STONE)));
+    public static final RegistryObject<BlockItem> NETHER_BLOCK_ITEM = ITEMS.register("nether_block_item", () -> new BlockItem(NETHER_BLOCK.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
+    public static final RegistryObject<Item> REDSTONEAXE= ITEMS.register("redstoneaxe", () -> new AxeItem(ItemTier.NETHERITE, 10, -1.5f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> REDSTONEPICKAXE = ITEMS.register("redstonepickaxe", () -> new PickaxeItem(ItemTier.NETHERITE, 1, -0.5f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> REDSTONESHOVEL = ITEMS.register("redstoneshovel", () -> new ShovelItem(ItemTier.NETHERITE, 2, -1.0f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> REDSTONESWORD = ITEMS.register("redstonesword", () -> new SwordItem(ItemTier.NETHERITE, 4, -0.3f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> REDSTONEHOE = ITEMS.register("redstonehoe", () -> new HoeItem(ItemTier.NETHERITE, 1, -0.5f, new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
 
 }
